6281abe65b4e7c04a4dc797f802fe8a7258aa387
Ashour100 <interinstitute5@gmail.com>
2022-04-22 01:00:53 +0200

    header added

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
     $data=[
         [
             'domanda' => 'How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?',
-            'risposta' => "<p>The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive. </p>
+            'risposta' => "<p>The recent <a href='#'> ruling by the Court of Justice of the European Union</a> has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive. </p>
 
             <p>Since this ruling was published on 13 May 2014, we've been working round the clock to comply. This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with public's right to know and distribute information.</p>
             
-            <p>If you have a removal request, please fill out this webform. You'll receive an automatic reply confirming that we have received your request. We will then assess your case—please note this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.</p>
+            <p>If you have a removal request, please fill out this <a href='#'> webform</a>. You'll receive an automatic reply confirming that we have received your request. We will then assess your case—please note this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.</p>
             
             <p>We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe it's important to respect the Court's judgment and are working hard to devise a process that complies with the law.</p>
             
@@ -19,9 +19,9 @@
 
             <p>We’re constantly working to ensure strong security, protect your privacy, and make Google even more effective and efficient for you. We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. We also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. So when it comes to the information you share with Google, you’re in control.</p>
             
-            <p>You can learn more about safety and security online, including how to protect yourself and your family online, at the Google Safety Center.</p>
+            <p>You can learn more about safety and security online, including how to protect yourself and your family online, at the <a href='#'>Google Safety Center</a>.</p>
             
-            <p>Learn more about how we keep your personal information private and safe — and put you in control.</p>",
+            <p><a href='#'>Learn more </a> about how we keep your personal information private and safe — and put you in control.</p>",
         ],
         [
             'domanda' => 'Why is my account associated with a country?',
@@ -43,15 +43,15 @@
 
             <p>Frequent travel doesn’t generally affect the country associated with your account. If you move to a new country, it can take about a year for your country association to update. </p>
             
-            <p>If the country associated with your account doesn’t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you’ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. Contact us if you think your country association is wrong.</p>",
+            <p>If the country associated with your account doesn’t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you’ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. <a href='#'>Contact us </a>if you think your country association is wrong.</p>",
         ],
         [
             'domanda' => "How can I remove information about myself from Google's search results?",
-            'risposta' => "<p>Google search results are a reflection of the content publicly available on the web. Search engines can't remove content directly from websites, so removing search results from Google wouldn't remove the content from the web. If you want to remove something from the web, you should contact the webmaster of the site the content is posted on and ask him or her to make a change. Additionally, if under European data protection law, you would like to request removal of certain information about you that appears in Google's search results, please click here. Once the content has been removed and Google has noted the update, the information will no longer appear in Google's search results. If you have an urgent removal request, you can also visit our help page for more information.</p>",
+            'risposta' => "<p>Google search results are a reflection of the content publicly available on the web. Search engines can't remove content directly from websites, so removing search results from Google wouldn't remove the content from the web. If you want to remove something from the web, you should contact the webmaster of the site the content is posted on and ask him or her to make a change. Additionally, if under European data protection law, you would like to request removal of certain information about you that appears in Google's search results, please <a href='#'>click here</a>. Once the content has been removed and Google has noted the update, the information will no longer appear in Google's search results. If you have an urgent removal request, you can also <a href='#'>visit our help page for more information</a>.</p>",
         ],
         [
             'domanda' => 'Are my search queries sent to websites when I click on Google Search results?',
-            'risposta' => "<p>In some cases, yes. When you click on a search result in Google Search, your web browser also may send the Internet address, or URL, of the search results page to the destination webpage as the Referrer URL. The URL of the search results page may sometimes contain the search query you entered. If you are using SSL Search (Google’s encrypted search functionality), under most circumstances, your search terms will not be sent as part of the URL in the Referrer URL. There are some exceptions to this behavior, such as if you are using some less popular browsers. More information on SSL Search can be found here. Search queries or information contained in the Referrer URL may be available via Google Analytics or an application programming interface (API). In addition, advertisers may receive information relating to the exact keywords that triggered an ad click.</p>",
+            'risposta' => "<p>In some cases, yes. When you click on a search result in Google Search, your web browser also may send the Internet address, or URL, of the search results page to the destination webpage as the <a href='#'>Referrer URL</a>. The URL of the search results page may sometimes contain the search query you entered. If you are using SSL Search (Google’s encrypted search functionality), under most circumstances, your search terms will not be sent as part of the URL in the Referrer URL. There are some exceptions to this behavior, such as if you are using some less popular browsers. More information on SSL Search can be found here. Search queries or information contained in the Referrer URL may be available via Google Analytics or an application programming interface (API). In addition, advertisers may receive information relating to the exact keywords that triggered an ad click.</p>",
         ],
     ];
 ?>
@@ -65,26 +65,35 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&family=Roboto&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <style>
-            h4{
-                font-family: 'Open Sans', sans-serif;
-                margin-top: 64px;
-            }
-            p,ol{
-                font-family: 'Roboto', sans-serif;
-                font-size: 14px;
-            }
-            p{
-                padding-top:20px;
-                margin-bottom: 10px;
-            }
-        </style>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+        <link rel="stylesheet" href="style.scss">
         <title>FAQ</title>
     </head>
     <body>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-8 mx-auto p-3">
+                <div class="col-12 header fixed-top p-0">
+                    <div class="top d-flex p-3 pb-0 w-100">
+                        <div class="left">
+                            <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="">
+                            <a href="#">Privacy and Terms</a>
+                        </div>
+                        <div class="right">
+                            <i class="fa-solid fa-grid"></i>
+                            <img src="img/user.png" alt="">
+                        </div>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Overview</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Terms of Service</a></li>
+                            <li><a href="#">Technologies</a></li>
+                            <li class="active"><a href="#" class="text-primary">FAQ</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-8 mx-auto p-3 mt-5 pt-5">
                 <?php
                     foreach($data as $content){
                         echo "<h4>" . $content['domanda'] . "</h4>";
