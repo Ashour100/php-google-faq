3d88fe08f11820cabf8a5d63872250c294d98ca6
Ashour100 <interinstitute5@gmail.com>
2022-04-21 23:00:26 +0200

    content organized

--- index.php
+++ index.php
@@ -3,41 +3,45 @@
     $data=[
         [
             'domanda' => 'How are you implementing the recent Court of Justice of the European Union (CJEU) decision on the right to be forgotten?',
-            'risposta' => "The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive.
+            'risposta' => "The recent ruling by the Court of Justice of the European Union has profound consequences for search engines in Europe. The court found that certain users have the right to ask search engines like Google to remove results for queries that include the person's name. To qualify, the results shown would need to be inadequate, irrelevant, no longer relevant, or excessive. <br>
 
-            Since this ruling was published on 13 May 2014, we've been working round the clock to comply. This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with public's right to know and distribute information.
+            Since this ruling was published on 13 May 2014, we've been working round the clock to comply. This is a complicated process because we need to assess each individual request and balance the rights of the individual to control his or her personal data with public's right to know and distribute information.<br>
             
-            If you have a removal request, please fill out this webform. You'll receive an automatic reply confirming that we have received your request. We will then assess your case—please note this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.
+            If you have a removal request, please fill out this webform. You'll receive an automatic reply confirming that we have received your request. We will then assess your case—please note this may take some time because we have already received many such requests. In evaluating your request, we will look at whether the results include outdated information about your private life. We'll also look at whether there's a public interest in the information remaining in our search results—for example, if it relates to financial scams, professional malpractice, criminal convictions or your public conduct as a government official (elected or unelected). These are difficult judgements and as a private organization, we may not be in a good position to decide on your case. If you disagree with our decision you can contact your local DPA.<br>
             
-            We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe it's important to respect the Court's judgment and are working hard to devise a process that complies with the law.
+            We look forward to working closely with data protection authorities and others over the coming months as we refine our approach. The CJEU's ruling constitutes a significant change for search engines. While we are concerned about its impact, we also believe it's important to respect the Court's judgment and are working hard to devise a process that complies with the law.<br>
             
             When you search for a name, you may see a notice that says that results may have been modified in accordance with data protection law in Europe. We’re showing this notice in Europe when a user searches for most names, not just pages that have been affected by a removal.",
         ],
         [
             'domanda' => 'How does Google protect my privacy and keep my information secure?',
-            'risposta' => "We know security and privacy are important to you – and they are important to us, too. We make it a priority to provide strong security and give you confidence that your information is safe and accessible when you need it.
+            'risposta' => "We know security and privacy are important to you – and they are important to us, too. We make it a priority to provide strong security and give you confidence that your information is safe and accessible when you need it.<br>
 
-            We’re constantly working to ensure strong security, protect your privacy, and make Google even more effective and efficient for you. We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. We also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. So when it comes to the information you share with Google, you’re in control.
+            We’re constantly working to ensure strong security, protect your privacy, and make Google even more effective and efficient for you. We spend hundreds of millions of dollars every year on security, and employ world-renowned experts in data security to keep your information safe. We also built easy-to-use privacy and security tools like Google Dashboard, 2-step verification and Ads Settings. So when it comes to the information you share with Google, you’re in control.<br>
             
-            You can learn more about safety and security online, including how to protect yourself and your family online, at the Google Safety Center.
+            You can learn more about safety and security online, including how to protect yourself and your family online, at the Google Safety Center.<br>
             
             Learn more about how we keep your personal information private and safe — and put you in control.",
         ],
         [
             'domanda' => 'Why is my account associated with a country?',
-            'risposta' => "Your account is associated with a country (or territory) in the Terms of Service so that we can determine two things:
-
-            The Google affiliate that provides the services, that processes your information, and that is responsible for complying with applicable privacy laws. Generally, Google offers its consumer services through either of two companies:
-            Google Ireland Limited, if you’re located in the European Economic Area (EU countries plus Iceland, Liechtenstein, and Norway) or Switzerland
-            Google LLC, based in the United States, for the rest of the world
-            The version of the terms that govern our relationship, which can vary depending on local laws
+            'risposta' => "Your account is associated with a country (or territory) in the Terms of Service so that we can determine two things:<br>
+            <ol>
+            <li>The Google affiliate that provides the services, that processes your information, and that is responsible for complying with applicable privacy laws. Generally, Google offers its consumer services through either of two companies:
+                <ol type='a'>
+                    <li>Google Ireland Limited, if you’re located in the European Economic Area (EU countries plus Iceland, Liechtenstein, and Norway) or Switzerland</li>
+                    <li>Google LLC, based in the United States, for the rest of the world</li>
+                </ol>
+            </li>
+            <li>The version of the terms that govern our relationship, which can vary depending on local laws</li>
+            </ol>
             Keep in mind that Google services are essentially the same regardless of the affiliate that provides the services or your country association.",
         ],
         [
             'domanda' => 'Determining the country associated with your account',
-            'risposta' => "When you create a new account, we associate your account with a country based on where you created your Google Account. For accounts at least a year old, we use the country from which you usually access Google services — typically where you’ve spent the most time in the last year.
+            'risposta' => "When you create a new account, we associate your account with a country based on where you created your Google Account. For accounts at least a year old, we use the country from which you usually access Google services — typically where you’ve spent the most time in the last year. <br>
 
-            Frequent travel doesn’t generally affect the country associated with your account. If you move to a new country, it can take about a year for your country association to update.
+            Frequent travel doesn’t generally affect the country associated with your account. If you move to a new country, it can take about a year for your country association to update. <br>
             
             If the country associated with your account doesn’t correspond to your country of residence, it could be because of a difference between your country of work and residence, because you’ve installed a Virtual Private Network (VPN) to mask your IP address, or because you live close to a territorial border. Contact us if you think your country association is wrong.",
         ],
@@ -62,11 +66,8 @@
     <body>
         <?php
             foreach($data as $content){
-                $tempAns=explode(".", $content['risposta'] );
                 echo "<h1>" . $content['domanda'] . "</h1>";
-                foreach($tempAns as $sentence){
-                    echo "<p>" . $sentence . ".</p>";
-                }
+                echo "<p>" . $content['risposta'] . "</p>";
             }
         ?>
     </body>
